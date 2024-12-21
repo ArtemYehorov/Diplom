aebf351dd4041a3855ca97d14cb6eda985c7984c
artem <artemyehorov@gmial.com>
2024-12-21 19:50:57 +0100

    last uppdate

--- person.py
+++ person.py
@@ -22,15 +22,34 @@ class Person:
         raise ValueError("Неверный формат даты!")
 
     def calculate_age(self):
-        """Вычисление возраста"""
-        end_date = self.death_date or datetime.date.today()
-        delta = end_date - self.birth_date
-        return delta.days // 365
+        """Расчёт возраста"""
+        today = self.death_date or datetime.date.today()
+        years = today.year - self.birth_date.year
+
+        if (today.month, today.day) < (self.birth_date.month, self.birth_date.day):
+            years -= 1
+        return years
+
+    def gender_check(self):
+        if self.gender == 'm':
+            return True
+        else:
+            return False
 
     def __str__(self):
-        """Форматированный вывод информации"""
-        gender_str = "мужчина" if self.gender == 'm' else "женщина"
+        """Вывод информации о персоне"""
+        gender_str = "мужчина" if self.gender_check() else "женщина"
+        birth_name = "Родился" if self.gender_check() else "Родилась"
+        death_name = "Умер" if self.gender_check() else "Умерла"
         age = self.calculate_age()
-        death_info = f"Умер(ла): {self.death_date}" if self.death_date else "Жив(а)"
-        return f"{self.first_name} {self.last_name} {self.patronymic}, {age} лет, {gender_str}. " \
-               f"Родился(лась): {self.birth_date}. {death_info}"
+
+        if age % 10 == 1 and age % 100 != 11:
+            age_str = f"{age} год"
+        elif 2 <= age % 10 <= 4 and not (12 <= age % 100 <= 14):
+            age_str = f"{age} года"
+        else:
+            age_str = f"{age} лет"
+
+        death_info = f"{death_name}: {self.death_date}" if self.death_date else "Жив(а)"
+        return f"{self.first_name} {self.last_name} {self.patronymic}, {age_str}, {gender_str}. " \
+               f"{birth_name}: {self.birth_date}. {death_info}"
